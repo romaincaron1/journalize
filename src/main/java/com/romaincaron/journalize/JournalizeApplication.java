6da3822c439366d2b50e85f83e474d28bef1e73c
romain caron <romain@awelty.com>
2024-02-09 14:50:54 +0100

    initial commit

---
+++ src/main/java/com/romaincaron/journalize/JournalizeApplication.java
@@ -0,0 +1,13 @@
+package com.romaincaron.journalize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JournalizeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JournalizeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/romaincaron/journalize/JournalizeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.romaincaron.journalize;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JournalizeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
